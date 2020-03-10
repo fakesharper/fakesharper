a157ebc4d58136240436bc11a63e1b5542142671
jaqra <jaqra@hotmail.com>
2020-03-11 01:20:58 +0300

    Added workspace helper

---
+++ src/workspace.ts
@@ -0,0 +1,34 @@
+import * as path from 'path';
+import * as vscode from 'vscode';
+
+export function selectFile(glob: vscode.GlobPattern, onSelect: ((path: string | undefined) => void)): void {
+	if (!vscode.workspace.workspaceFolders) {
+		vscode.window.showWarningMessage('There is no open folder.');
+		return;
+	}
+
+	vscode.workspace.findFiles(glob, '**/node_modules/**')
+		.then(value => {
+			if (value.length === 0) {
+				onSelect(undefined);
+			} else if (value.length === 1) {
+				onSelect(value[0].fsPath);
+			} else {
+				const items: vscode.QuickPickItem[] = value.map(x => ({
+					label: path.basename(x.fsPath),
+					description: x.fsPath
+				}));
+
+				vscode.window.showQuickPick(items, { placeHolder: 'Select file' })
+					.then(value => {
+						if (value) {
+							onSelect(value.description as string);
+						}
+					});
+			}
+		});
+}
+
+export function selectSolutionFile(onSelect: ((path: string | undefined) => void)): void {
+	selectFile('**/*.sln', onSelect);
+}
