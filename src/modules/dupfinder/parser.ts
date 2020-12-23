dcc95f63a18f5f1a9c2ce595c8a907ea847973ed
jaqra <jaqra@hotmail.com>
2020-12-24 00:31:42 +0300

    Fix error when 0 or 1 diplicate found

--- src/modules/dupfinder/parser.ts
+++ src/modules/dupfinder/parser.ts
@@ -21,37 +21,42 @@ export function parsefile(path: string): DuplicatesReport {
 		duplicates: []
 	};
 
-	// TODO: Test for 0 and 1 duplicate.
-	for (let i = 0; i < json.DuplicatesReport.Duplicates.Duplicate.length; i++) {
-		const duplicate: Duplicate = {
-			cost: parseInt(json.DuplicatesReport.Duplicates.Duplicate[i].attributes["@_Cost"]),
-			fragment1: {
-				filePath: '',
-				fileName: json.DuplicatesReport.Duplicates.Duplicate[i].Fragment[0].FileName,
-				offsetRange: {
-					start: json.DuplicatesReport.Duplicates.Duplicate[i].Fragment[0].OffsetRange.attributes["@_Start"],
-					end: json.DuplicatesReport.Duplicates.Duplicate[i].Fragment[0].OffsetRange.attributes["@_End"]
-				},
-				lineRange: {
-					start: json.DuplicatesReport.Duplicates.Duplicate[i].Fragment[0].LineRange.attributes["@_Start"],
-					end: json.DuplicatesReport.Duplicates.Duplicate[i].Fragment[0].LineRange.attributes["@_End"]
-				}
-			},
-			fragment2: {
-				filePath: '',
-				fileName: json.DuplicatesReport.Duplicates.Duplicate[i].Fragment[1].FileName,
-				offsetRange: {
-					start: json.DuplicatesReport.Duplicates.Duplicate[i].Fragment[1].OffsetRange.attributes["@_Start"],
-					end: json.DuplicatesReport.Duplicates.Duplicate[i].Fragment[1].OffsetRange.attributes["@_End"]
-				},
-				lineRange: {
-					start: json.DuplicatesReport.Duplicates.Duplicate[i].Fragment[1].LineRange.attributes["@_Start"],
-					end: json.DuplicatesReport.Duplicates.Duplicate[i].Fragment[1].LineRange.attributes["@_End"]
-				}
-			}
-		};
+	if (json.DuplicatesReport.Duplicates !== '') {
+		if (json.DuplicatesReport.Duplicates.Duplicate && json.DuplicatesReport.Duplicates.Duplicate.length === undefined) {
+			json.DuplicatesReport.Duplicates.Duplicate = [json.DuplicatesReport.Duplicates.Duplicate];
+		}
 
-		report.duplicates.push(duplicate);
+		for (let i = 0; i < json.DuplicatesReport.Duplicates.Duplicate.length; i++) {
+			const duplicate: Duplicate = {
+				cost: parseInt(json.DuplicatesReport.Duplicates.Duplicate[i].attributes["@_Cost"]),
+				fragment1: {
+					filePath: '',
+					fileName: json.DuplicatesReport.Duplicates.Duplicate[i].Fragment[0].FileName,
+					offsetRange: {
+						start: json.DuplicatesReport.Duplicates.Duplicate[i].Fragment[0].OffsetRange.attributes["@_Start"],
+						end: json.DuplicatesReport.Duplicates.Duplicate[i].Fragment[0].OffsetRange.attributes["@_End"]
+					},
+					lineRange: {
+						start: json.DuplicatesReport.Duplicates.Duplicate[i].Fragment[0].LineRange.attributes["@_Start"],
+						end: json.DuplicatesReport.Duplicates.Duplicate[i].Fragment[0].LineRange.attributes["@_End"]
+					}
+				},
+				fragment2: {
+					filePath: '',
+					fileName: json.DuplicatesReport.Duplicates.Duplicate[i].Fragment[1].FileName,
+					offsetRange: {
+						start: json.DuplicatesReport.Duplicates.Duplicate[i].Fragment[1].OffsetRange.attributes["@_Start"],
+						end: json.DuplicatesReport.Duplicates.Duplicate[i].Fragment[1].OffsetRange.attributes["@_End"]
+					},
+					lineRange: {
+						start: json.DuplicatesReport.Duplicates.Duplicate[i].Fragment[1].LineRange.attributes["@_Start"],
+						end: json.DuplicatesReport.Duplicates.Duplicate[i].Fragment[1].LineRange.attributes["@_End"]
+					}
+				}
+			};
+
+			report.duplicates.push(duplicate);
+		}
 	}
 
 	report.duplicates = report.duplicates.sort((x, y) => y.cost - x.cost);
