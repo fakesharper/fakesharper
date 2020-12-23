266e7a4ec0845ff7a2267cada550f3db9a813712
jaqra <jaqra@hotmail.com>
2020-12-24 00:30:40 +0300

    Make install jb synchronous

--- src/utils/jetbrainsinstaller.ts
+++ src/utils/jetbrainsinstaller.ts
@@ -1,5 +1,5 @@
 import { INSPECTION_COMMAND, JB_INSTALLATION_COMMAND } from "../constants";
-import { spawn } from 'child_process';
+import { execSync } from 'child_process';
 import * as vscode from 'vscode';
 
 export class JetBrainsInstaller {
@@ -10,36 +10,22 @@ export class JetBrainsInstaller {
 
 	verifyInstallation() {
 		this.output.appendLine('');
-		const cp = spawn(INSPECTION_COMMAND, ["--version"], { shell: true });
-
-		cp.stdin?.addListener('data', message => this.output.append(message.toString()));
-		cp.stdout?.addListener('data', message => this.output.append(message.toString()));
-		cp.stderr?.addListener('data', message => this.output.append(message.toString()));
-
-		cp.on('exit', code => {
-			if (code !== 0) {
-				this.runInstallation();
-			}
-			else {
-				this.output.appendLine('Jetbrains already installed.');
-			}
-		});
+		try{
+			execSync(`${INSPECTION_COMMAND} --version`).toString();
+		} catch {
+			this.runInstallation();
+		}
 	}
 
 	private runInstallation() {
 		this.output.appendLine('Running Jetbrains installation');
-		const cp = spawn(JB_INSTALLATION_COMMAND, { shell: true });
 
-		cp.stdin?.addListener('data', message => this.output.append(message.toString()));
-		cp.stdout?.addListener('data', message => this.output.append(message.toString()));
-		cp.stderr?.addListener('data', message => this.output.append(message.toString()));
-
-		cp.on('exit', code => {
-			if (code !== 0) {
-				this.output.appendLine('Problems to install Jetbrains.');
-			}
-
+		try{
+			const output = execSync(JB_INSTALLATION_COMMAND).toString();
+			this.output.append(output);
 			this.output.appendLine('Done.');
-		});
+		} catch (error) {
+			this.output.append(error.toString());
+		}
 	}
 }
