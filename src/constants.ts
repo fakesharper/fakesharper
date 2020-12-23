30394410466a6d063dae34b722c5277a2d1005c2
jaqra <jaqra@hotmail.com>
2020-12-24 00:31:05 +0300

    :lipstick:

--- src/constants.ts
+++ src/constants.ts
@@ -5,5 +5,4 @@ export const JB_COMMAND: string = 'jb';
 export const DUPFINDER_COMMAND: string = JB_COMMAND + ' dupfinder';
 export const CLEANCODE_COMMAND: string = JB_COMMAND + ' cleanupcode';
 export const INSPECTION_COMMAND: string = JB_COMMAND + ' inspectcode';
-export const JB_INSTALLATION_COMMAND: string = 'dotnet tool install -g Jetbrains.Resharper.globaltools';
-
+export const JB_INSTALLATION_COMMAND: string = 'dotnet tool install -g JetBrains.ReSharper.GlobalTools';
